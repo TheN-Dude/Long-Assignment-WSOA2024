9cabb1af85d625cd1cc1cba0735f43e5e0763bb1
TheN-Dude <nnmurire77@gmail.com>
2024-05-14 15:12:45 +0200

    Revert "Iteration Changes"
    This reverts commit 4c5031d020877fd76e640749d0f53708183d1d49.

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -1,7 +1,10 @@
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class PlayerSetup : MonoBehaviour
 {
